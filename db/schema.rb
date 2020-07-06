dde245b9afbf4008fbca5cf162021b084386a5a3
github username <github email address>
2020-07-06 11:50:27 -0700

    uses rails api g

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_172108) do
+ActiveRecord::Schema.define(version: 2020_07_06_184444) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "journals", force: :cascade do |t|
     t.string "title"
