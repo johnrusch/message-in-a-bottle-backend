c96bb1eb0e3baff07629c33a8ada9ee0d3237ddc
johnrusch <johnrusch7@gmail.com>
2020-07-09 10:53:33 -0700

    fixes user sign up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,9 +7,10 @@ class Api::V1::UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
-        if @user.valid?
-            render json: {user:
-        UserSerializer.new(@user) }, status: :created
+        byebug
+        if @user.valid? && @user.authenticate(params[:password])
+            token = issue_token(@user)
+            render json: {id: @user.id, name: @user.name, jwt: token}
         else
             render json: { error: 'Failed to create user' }, status: :not_acceptable
         end
@@ -17,6 +18,6 @@ class Api::V1::UsersController < ApplicationController
 
     private
     def user_params
-        params.permit(:name, :password)
+        params.permit(:user, :name, :password)
     end
 end
